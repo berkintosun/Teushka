2ab608e9064b9bb90e77f92ce5f07b2012a06f07
Tea Dulellari <teadulellari@gmail.com>
2022-05-29 18:33:14 +0300

    initial boilerplate code

---
+++ src/main/java/com/berkin/teushka/TeushkaBotApplication.java
@@ -0,0 +1,13 @@
+package com.berkin.teushka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeushkaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeushkaBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/berkin/teushka/TeushkaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkin.teushka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeushkaBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
